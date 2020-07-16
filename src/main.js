50789473a6857582c9316e68191b614d5dfe66ad
yangfeng_ZR <yangfeng12345@139.com>
2020-07-16 17:14:15 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -17,6 +17,6 @@ new Vue({
   el: '#app',//挂载的element
   router,//路由，指向引入文件中的routes:[]
   store, //状态管理
-  components: { App:App},  //注册的组件需要在顶部引入,这里是局部组件，只能在当前页面中使用
+  components: {APP},  //注册的组件需要在顶部引入,这里是局部组件，只能在当前页面中使用
   template: '<App/>'  // render优先级 > template 优先级 >el优先级, template此处使用App.vue里面的模板，代替el绑定的dom元素
 })
